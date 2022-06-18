5565e0ee71c36b917ae3d871caebc60a9f8920cc
Martin Cortes <martin.cortes@blackboard.com>
2022-06-18 14:13:50 -0500

    form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import './App.css';
 import Matches from './components/Matches';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { Autocomplete, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Autocomplete, Button, Chip, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 
 const divStyle = {
   display: "flex",
@@ -24,16 +24,36 @@ const players = [
 ];
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  // const { register, handleSubmit } = useForm();
   const [when, setWhen] = React.useState<Date | null>(new Date());
+  const [type, setType] = React.useState("");
+  const [localTeam, setLocalTeam] = React.useState([]);
+  const [visitorTeam, setVisitorTeam] = React.useState([]);
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      name: '',
+      when: new Date(),
+      where: '',
+      matchType: '',
+      localTeam: [],
+      visitorTeam: [],
+    }
+  });
 
   const onSubmit = (data) => {
     console.log(data);
-    axios.post("https://3jgliispfg.execute-api.us-east-1.amazonaws.com/dev/matches", { data })
-      .then((response) => {
-        console.log(response);
-        console.log(response.data);
-      });
+    // axios.post("https://3jgliispfg.execute-api.us-east-1.amazonaws.com/dev/matches", { data })
+    //   .then((response) => {
+    //     console.log(response);
+    //     console.log(response.data);
+    //   });
+  };
+
+  const handleChangeType = (event: SelectChangeEvent) => {
+    console.log('asdasds');
+
+    setType(event.target.value as string);
   };
 
   return (
@@ -41,35 +61,110 @@ function App() {
       <div className="App">
         <h1>Create match</h1>
         <form onSubmit={handleSubmit(onSubmit)} style={divStyle}>
-          <TextField label="Name" type={"text"} variant="outlined" {...register("name")} />
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DateTimePicker
-              renderInput={(props) => <TextField variant="outlined" {...props} />}
-              label="When"
-              value={when}
-              onChange={(newValue) => {
-                setWhen(newValue);
+          <Controller
+            name="name"
+            control={control}
+            render={({ field }) => <TextField {...field} label="Name" type={"text"} variant="outlined" />}
+          />
+          <Controller
+            name="when"
+            control={control}
+            render={({ field }) =>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                  renderInput={(props) => <TextField required variant="outlined" {...props} />}
+                  label="When"
+                  value={when}
+                  onChange={(newValue) => {
+                    setWhen(newValue);
+                  }}
+                />
+              </LocalizationProvider>
+            }
+          />
+          <Controller
+            name="where"
+            control={control}
+            render={({ field }) => <TextField required label="Where" type={"text"} variant="outlined" {...field} />}
+          />
+          <Controller
+            name="matchType"
+            control={control}
+            render={({ field }) =>
+              <FormControl required fullWidth>
+                <InputLabel id="match-type">Match type</InputLabel>
+                <Select
+                  labelId="match-type"
+                  {...field}
+                  value={type}
+                  label="Match type"
+                  onChange={handleChangeType}
+                >
+                  <MenuItem value={5}>5</MenuItem>
+                  <MenuItem value={8}>8</MenuItem>
+                  <MenuItem value={11}>11</MenuItem>
+                </Select>
+              </FormControl>}
+          />
+          <Controller
+            name="localTeam"
+            control={control}
+            render={({ field }) =>
+              <Autocomplete
+                multiple
+                limitTags={5}
+                freeSolo
+                {...field}
+                value={localTeam}
+                onChange={(event, newValue) => {
+                  setLocalTeam([...newValue]);
+                }}
+                options={players.map((option) => option["label"])}
+                renderTags={(value: readonly string[], getTagProps) =>
+                  value.map((option: string, index: number) => (
+                    <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    required={localTeam.length === 0}
+                    variant="outlined"
+                    label="Local Team"
+                    placeholder="Players"
+                  />
+                )}
+              />}
+          />
+
+          <Controller
+            name="visitorTeam"
+            control={control}
+            render={({ field }) => <Autocomplete
+              multiple
+              limitTags={5}
+              freeSolo
+              {...field}
+              value={visitorTeam}
+              onChange={(event, newValue) => {
+                setVisitorTeam([...newValue]);
               }}
-            />
-          </LocalizationProvider>
-          <TextField label="Where" type={"text"} variant="outlined" {...register("field")} />
-          <FormControl fullWidth>
-            <InputLabel id="match-type">Match type</InputLabel>
-            <Select labelId="match-type" label="Match type" value={8} {...register("matchType")}>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={8}>8</MenuItem>
-              <MenuItem value={11}>11</MenuItem>
-            </Select>
-          </FormControl>
-          <Autocomplete
-            multiple
-            limitTags={5}
-            options={players}
-            getOptionLabel={(option) => option.title}
-            renderInput={(params) => (
-              <TextField {...params} label="Players" placeholder="Players" />
-            )}
-            sx={{ width: '500px' }}
+              options={players.map((option) => option["label"])}
+              renderTags={(value: readonly string[], getTagProps) =>
+                value.map((option: string, index: number) => (
+                  <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                ))
+              }
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  required
+                  variant="outlined"
+                  label="Visitor Team"
+                  placeholder="Players"
+                />
+              )}
+            />}
           />
           <Button type="submit" variant="contained">Create</Button>
         </form>
